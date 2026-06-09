beec997d0ef9e55465bd28f021e273da3984ea5f
ggalanteCode <ggalante3@gmail.com>
2026-06-09 19:17:09 +0200

    render public playlists and allow to open them but not to delete them

--- js/pages/playlists.js
+++ js/pages/playlists.js
@@ -3,7 +3,10 @@ import {
     createPlaylist,
     deletePlaylist
  } from "../api/playlist-api.js";
+import { renderPlaylists, bindOpenButtons, bindDeleteButtons } from "./playlist-ui.js";
 import { isLoggedIn } from "../utils/auth-utils.js";
+
+console.log("MY PLAYLISTS PAGE");
 
 document.addEventListener("DOMContentLoaded", async () => {
 
@@ -20,172 +23,50 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 });
 
-async function loadPlaylists() {
+export async function loadPlaylists() {
 
     try {
 
         const playlists = await getMyPlaylists();
 
-        renderPlaylists(playlists);
+        renderPlaylists(playlists, true);
+        bindOpenButtons(false);
+        bindDeleteButtons(loadPlaylists);
 
     } catch (error) {
 
         console.error(error);
 
     }
-
-}
-
-function renderPlaylists(playlists) {
-
-    const container =
-        document.getElementById(
-            "playlists-container"
-        );
-
-    container.innerHTML = "";
-
-    if (playlists.length === 0) {
-
-        container.innerHTML = `
-            <p>No playlists found.</p>
-        `;
-
-        return;
-    }
-
-    playlists.forEach(playlist => {
-
-        container.innerHTML += `
-            <div class="card mb-3">
-
-                <div class="card-body">
-
-                    <h5>
-                        ${playlist.title}
-                    </h5>
-
-                    <p>
-                        ${playlist.description ?? ""}
-                    </p>
-
-                    <button
-                        class="btn btn-success open-playlist-btn"
-                        data-playlist-id="${playlist.id}">
-                        Open
-                    </button>
-
-                    <button
-                        class="btn btn-danger delete-playlist-btn"
-                        data-playlist-id="${playlist.id}">
-                        Delete
-                    </button>
-
-                </div>
-
-            </div>
-        `;
-    });
-
-    bindOpenButtons();
-
-    bindDeleteButtons();
-
-}
-
-function bindOpenButtons() {
-
-    document
-        .querySelectorAll(".open-playlist-btn")
-        .forEach(button => {
-
-            button.addEventListener("click", () => {
-
-                const playlistId =
-                    button.dataset.playlistId;
-
-                window.location.href =
-                    `playlist.html?id=${playlistId}`;
-
-            });
-
-        });
-
-}
-
-function bindDeleteButtons() {
-
-    document
-        .querySelectorAll(".delete-playlist-btn")
-        .forEach(button => {
-
-            button.addEventListener("click", async () => {
-
-                const playlistId =
-                    button.dataset.playlistId;
-
-                const confirmed = confirm(
-                    "Are you sure you want to delete this playlist?"
-                );
-
-                if (!confirmed) {
-                    return;
-                }
-
-                try {
-
-                    await deletePlaylist(
-                        playlistId
-                    );
-
-                    alert(
-                        "Playlist deleted successfully"
-                    );
-
-                    await loadPlaylists();
-
-                } catch (error) {
-
-                    console.error(error);
-
-                    alert(
-                        "Unable to delete playlist"
-                    );
-                }
-
-            });
-
-        });
-
 }
 
 //salvataggio della nuova playlist con la modale
-document
-    .getElementById("savePlaylistBtn")
-    .addEventListener("click", async () => {
+const savePlaylistBtn =
+    document.getElementById("savePlaylistBtn");
 
-        const title =
-            document.getElementById(
-                "playlistTitle"
-            ).value;
+if (savePlaylistBtn) {
 
-        const description =
-            document.getElementById(
-                "playlistDescription"
-            ).value;
+    savePlaylistBtn.addEventListener(
+        "click",
+        async () => {
 
-        const request = {
-            title,
-            description
-        };
+            const title =
+                document.getElementById("playlistTitle").value;
 
-        console.log(request);
+            const description =
+                document.getElementById("playlistDescription").value;
 
-        // qui chiamerai il backend
-        await createPlaylist(request);
+            const request = {
+                title,
+                description,
+                isPublic: document.getElementById("playlistPublic").checked
+            };
 
-        $("#createPlaylistModal").modal("hide");
+            await createPlaylist(request);
 
-        await loadPlaylists();
+            $("#createPlaylistModal").modal("hide");
 
-    });
+            await loadPlaylists();
+        }
+    );
+}
